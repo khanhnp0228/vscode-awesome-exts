d78fc9d2ae70648db7b9078d1dd8b118098dccaa
Khanh Nguyen <khanhnp.ot@gmail.com>
2025-09-29 21:10:13 +0700

    Update: Local 20250929_2110

--- extensions-import-react-next.sh
+++ extensions-import-react-next.sh
@@ -1,5 +1,7 @@
 # !/bin/bash
 
-cat packages-default.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
-cat packages-frontend.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
-cat packages-react.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
+# cat packages-default.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
+# cat packages-frontend.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
+# cat packages-react.list | xargs -L 1 code --profile "React/Next.js"  --force --install-extension
+
+code --profile "React/Next.js"  --force --install-extension wallabyjs.quokka-vscode
